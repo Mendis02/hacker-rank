b0c601b163897e7ece42c39a764ef607bd1d8a61
Mendis02 <103174136+Mendis02@users.noreply.github.com>
2022-06-18 23:44:33 +0530

    Add files via upload

---
+++ hacker-rank-lists.py
@@ -0,0 +1,40 @@
+if __name__ == '__main__':
+    n = int(input('how many commands? '))
+    i = 1
+    the_list = []
+    while i <= n:
+    # used a 'while' loop , can also use a 'for' loop (which might be easier)
+
+        operation = input('command: ')
+        backstage = operation.split()
+    #inserting the input into a list
+
+        if backstage[0] == "print" :
+            print(the_list)
+            i = i+1
+        else:
+            if backstage[0] == "append":
+                the_list.append(backstage[1])
+                i = i + 1
+            else:
+                if backstage[0] == "sort":
+                    the_list.sort()
+                    i = i + 1
+                else:
+                    if backstage[0] == "pop":
+                        the_list.pop()
+                        i = i + 1
+                    else:
+                        if backstage[0] == "reverse":
+                            the_list.reverse()
+                            i = i + 1
+                        else:
+                            if backstage[0] == "remove":
+                                the_list.remove(backstage[1])
+                                i = i + 1
+                            else:
+                                if backstage[0] == "insert":
+                                    the_list.insert(backstage[2] , backstage[1])
+                                    i = i + 1
+                                    break
+
