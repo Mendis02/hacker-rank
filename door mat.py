cfb009f32568f0267af72113e3cbbf0695baa15c
Mendis02 <103174136+Mendis02@users.noreply.github.com>
2022-07-01 14:06:43 +0530

    Add files via upload

---
+++ door mat.py
@@ -0,0 +1,22 @@
+n = int(input())
+m = (n*3)
+
+if (n%2) == 1 :
+    x = int((n-1)/2) #number of top/bottom rows
+
+    y = int(((m-7)/2)) #number of '-'s in one part of the middle row
+    c = "-"
+    d = ".|."
+
+    #top part
+    for i in range(x+1):
+        print(i*d)
+
+    #middle part
+    print (f"{(y*c)}WELCOME{y*c}")
+
+    #bottom part
+    for i in range(x+1):
+        print(i*d)
+
+
