a539b5ccf5c41ea624de414c61c85c2f0de442bd
Mendis02 <103174136+Mendis02@users.noreply.github.com>
2022-07-01 15:49:49 +0530

    Update door mat.py

--- door mat.py
+++ door mat.py
@@ -1,7 +1,7 @@
-#still a work in progress
+n, m = input().split()
+n = int(n)
+m = int(m)
 
-n = int(input())
-m = (n*3)
 
 if (n%2) == 1 :
     x = int((n-1)/2) #number of top/bottom rows
@@ -11,14 +11,14 @@ if (n%2) == 1 :
     d = ".|."
 
     #top part
-    for i in range(x+1):
-        print(i*d)
+    for i in range(1,x+1):
+        print(int((m-(6*i-3))/2)*c +(2*i-1)*d+int((m-(6*i-3))/2)*c )
 
     #middle part
     print (f"{(y*c)}WELCOME{y*c}")
 
     #bottom part
-    for i in range(x+1):
-        print(i*d)
+    for i in range(1,x+1):
+        print((3*i)*c+int((m-6*i)/3)*d+(3*i)*c)
 
 
