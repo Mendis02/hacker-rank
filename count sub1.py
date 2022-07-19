19ffa3020e59361a76d69afe0dd05d010193a0e6
Mendis02 <103174136+Mendis02@users.noreply.github.com>
2022-07-19 13:18:02 +0530

    Update count sub1.py

--- count sub1.py
+++ count sub1.py
@@ -1,12 +1,12 @@
+import re
+
 def count_substring(string, sub_string):
-
-    x = string.count(sub_string)
+    x = len(re.findall(f'(?={sub_string})',string))
     return x
-
-
+ 
 if __name__ == '__main__':
     string = input().strip()
     sub_string = input().strip()
-
+    
     count = count_substring(string, sub_string)
     print(count)
